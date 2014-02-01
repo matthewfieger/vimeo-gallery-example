3c1a3bc028fb882e1d54333156a44928cc633e3e
Matthew Fieger <matthewfieger@Matthews-MacBook-Pro.local>
2014-02-01 15:16:24 -0500

    initial commit

---
+++ oembed-response.js
@@ -0,0 +1,19 @@
+switchVideo({
+    "type": "video",
+    "version": "1.0",
+    "provider_name": "Vimeo",
+    "provider_url": "https:\/\/vimeo.com\/",
+    "title": "LEGO Space Shuttle Time Lapse",
+    "author_name": "Brad Dougherty",
+    "author_url": "http:\/\/vimeo.com\/brad",
+    "is_plus": "0",
+    "html": "<iframe src=\"\/\/player.vimeo.com\/video\/50873112\" width=\"504\" height=\"280\" frameborder=\"0\" title=\"LEGO Space Shuttle Time Lapse\" webkitallowfullscreen mozallowfullscreen allowfullscreen><\/iframe>",
+    "width": 504,
+    "height": 280,
+    "duration": 36,
+    "description": "",
+    "thumbnail_url": "http:\/\/b.vimeocdn.com\/ts\/350\/557\/350557738_295.jpg",
+    "thumbnail_width": 295,
+    "thumbnail_height": 166,
+    "video_id": 50873112
+})
---
+++ vimeo-gallery.js
@@ -0,0 +1,69 @@
+/*************************************************************************
+************************************
+************************************
+* Vimeo Simple API
+*
+* The Simple API is a really easy way to get information about public videos, users, groups, channels, albums, and activity.
+*
+* http://developer.vimeo.com/apis/simple
+************************************
+************************************
+**************************************************************************/
+
+var apiEndpoint = 'http://vimeo.com/api/v2/';
+var oEmbedEndpoint = 'http://vimeo.com/api/oembed.json'
+var oEmbedCallback = 'switchVideo';
+var videosCallback = 'setupGallery';
+var vimeoUsername = 'brad';
+
+
+$(document).ready(function() {
+        // Load a JSON file from the Vimeo Simple API Server using a GET HTTP request, then execute it.
+        // Vimeo wraps the response in a callback function called 'setupGallery'
+        // console.log("Requested videos from " + apiEndpoint + vimeoUsername + '/videos.json?callback=' + videosCallback);
+        $.getScript(apiEndpoint + vimeoUsername + '/videos.json?callback=' + videosCallback);
+});
+
+
+function getVideo(url) {
+        // Load a JSON file from the Vimeo Simple API Server using a GET HTTP request, then execute it.
+        // Vimeo wraps the response in a callback function called 'switchVideo'
+        // console.log("Requested embed code from " + oEmbedEndpoint + '?url=' + url + '&width=504&height=280&callback=' + oEmbedCallback);
+        $.getScript(oEmbedEndpoint + '?url=' + url + '&width=504&height=280&callback=' + oEmbedCallback);
+}
+
+
+function setupGallery(videos) {
+
+        // Set the user's thumbnail and the page title
+        // console.log(videos)
+        $('#stats').prepend('<img id="portrait" src="' + videos[0].user_portrait_medium + '" />');
+        $('#stats h2').text(videos[0].user_name + "'s Videos");
+
+        // Load the first video
+        // console.log("Request embed code for " + videos[0].url);
+        // console.log(getVideo(videos[0].url).html);
+        getVideo(videos[0].url);
+
+        // Add the videos to the gallery
+        for (var i = 0; i < videos.length; i++) {
+                // console.log(videos[i]);
+                var html = '<li><a href="' + videos[i].url + '"><img src="' + videos[i].thumbnail_large + '" class="thumb" />';
+                html += '<p>' + videos[i].title + '</p></a></li>';
+                $('#thumbs ul').append(html);
+        }
+
+        // Switch to the video when a thumbnail is clicked
+        $('#thumbs a').click(function(event) {
+                event.preventDefault();
+                getVideo(this.href);
+                return false;
+        });
+
+}
+
+
+function switchVideo(video) {
+        $('#embed').html(unescape(video.html));
+}
+
